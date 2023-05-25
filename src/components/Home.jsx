576ebde65f5701b9187f8326995aa1448c3906a6
prajwal-shindee <prajwalshinde985@gmailcom>
2023-05-25 22:50:36 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,117 @@
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '4xl',
+};
+
+const Home = () => {
+  return (
+    <Box>
+      <MyCarousel />
+
+      <Container maxW={'container.xl'} minH={'100vh'} p="16">
+        <Heading
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m="auto"
+          
+        >
+          Services
+        </Heading>
+
+        <Stack
+          h="full"
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={['4', '16']}
+            textAlign={'center'}
+          >
+        Game Library: A comprehensive collection of video games that users can browse, search, and purchase.
+
+        Online Gaming: The website could host online multiplayer games, allowing users to play with friends or other players from around the world.
+
+        Game Reviews: A section where users can read reviews and ratings of different games written by experts or fellow gamers.
+
+        Gaming News: A section that keeps users up-to-date with the latest gaming news, releases, and events.
+
+        Gaming Community: A platform for gamers to connect, share their experiences, and participate in discussions about their favorite games.
+
+        Game Walkthroughs and Tips: A section where users can find guides, tutorials, and tips for different games to help them progress through the game.
+
+        Virtual Currency: The website could offer a virtual currency system, allowing users to purchase games, virtual items, and other services.
+
+        Mobile Gaming: The website could offer a mobile app version of the platform, allowing users to play games on the go.
+
+        Game Streaming: A feature that allows users to live-stream their gaming sessions and interact with their viewers in real-time.
+
+        E-Sports Tournaments: The website could host online e-sports tournaments, allowing gamers to compete against each other for prizes and bragging rights.
+
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={1000}
+    showStatus={false}
+    showThumbs={false}
+    
+  >
+    <Box w="full" h={'100vh'}>
+      <Image src={img1} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Be Classy
+      </Heading>
+    </Box>
+    <Box w="full" h={'100vh'}>
+      <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+        Its All About Fun
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+       PRO PLAYER
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img4} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        One Mistake Game Over
+      </Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
